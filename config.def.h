8f67e13ed73f44c18f8e3f2aa44ecc881804a0f7
rajik470 <dmitryrajik@gmail.com>
2023-04-05 18:21:10 +0300

    font fix

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=22:antialias=true:autohint=true";
+static char *font = "JetBrains Mono Medium Nerd Font Complete:pixelsize=22:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
